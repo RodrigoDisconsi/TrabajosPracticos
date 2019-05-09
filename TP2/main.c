c663faff0fa103ec0e1aae7d61d0cfddf95f3dd6
rodrigodisconsi@gmail.com <RodrigoDisconsi@gmail.com>
2019-05-09 20:30:14 -0300

    Segundo commit

--- TP2/main.c
+++ TP2/main.c
@@ -4,7 +4,7 @@
 #include "disconsi.h"
 #include "menu.h"
 #include "ArrayEmpleado.h"
-#define TAM 7
+#define TAM 1000
 
 int main()
 {
@@ -14,18 +14,8 @@ int main()
     int todoOk;
     int legajo;
     legajo = 10000;
-    eEmpleado lista[TAM] =
-    {
-        {10008, "Rodrigo",   "Disconsi",   300000.000, 3, 1},
-        {10009, "Ivan",    "Gonzales",   400000.000, 2, 1},
-        {10010, "Harry",   "Fernandes",  450.000, 1, 1},
-        {10011, "Lucas",   "Liciardi",   550.000, 2, 0},
-        {10012, "Fernando","Laurido",    650.000, 1, 1},
-        {10113, "Mariano", "Perez",      750.000, 4, 1},
-        {10114, "Pablo",   "Calamardo",  850.000, 3, 1}
-
-    };
-    //inicializarEmpleados(lista, TAM);
+    eEmpleado lista[TAM];
+    inicializarEmpleados(lista, TAM);
     do
     {
         switch(menuPrincipal())
